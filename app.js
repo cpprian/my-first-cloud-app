ba88fb9bb9dfbfc00876143117eadb9d38872bc7
cpprian <cpprian456@gmail.com>
2023-03-29 16:29:47 +0200

    add auth

--- app.js
+++ app.js
@@ -3,8 +3,52 @@ var app = express();
 
 const port = 8080;
 
+const CLIENT_ID = OAuth2Data.web.client_id;
+const CLIENT_SECRET = OAuth2Data.web.client_secret; 
+const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
+
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+var authed = false;
+
 app.get('/', (req, res) => {
-    res.send('Hello World! My first google cloud app!');
+    if (!authed) {
+        const url = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: 'https://www.googleapis.com/auth/userinfo.profile'
+        });
+        console.log(url)
+        res.redirect(url);
+    } else {
+        var oauth2 = google.oauth2({ auth: oAuth2Client, version: 'v2' });
+        oauth2.userinfo.me.get(function (err, response) {
+            if (err) {
+                console.log('Error occured')
+                console.log(err);
+            } else {
+                console.log(response.data);
+                res.send(response.data);
+            }
+            res.send('Logged in');
+        });
+    }
+})
+
+app.get('/auth/google/callback', function (req, res) {
+    const code = req.query.code
+    if (code) {
+        // Get an access token based on our OAuth code
+        oAuth2Client.getToken(code, function (err, tokens) {
+            if (err) {
+                console.log('Error authenticating')
+                console.log(err);
+            } else {
+                console.log('Successfully authenticated');
+                oAuth2Client.setCredentials(tokens);
+                authed = true;
+                res.redirect('/')
+            }
+        });
+    }
 });
 
 app.listen(port, () => {
